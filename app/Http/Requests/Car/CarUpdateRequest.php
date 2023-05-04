8acc3e26885e64025172fa68fcc08b1011721f57
cupskydev <yusuf.wandana1@gmail.com>
2023-05-04 20:12:49 +0700

    Validation request for create and update car data

---
+++ app/Http/Requests/Car/CarUpdateRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Car;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CarUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "name"  => "required|string|max:255",
+            "brand" => "required|string|max:255",
+            "color" => "required|string|max:255",
+            "image_url" => "required|string",
+            "year"  => "required|numeric|digits:4",
+            "price" => "required|numeric|min:1",
+            "description" => "required|string",
+        ];
+    }
+}
